c05f83309349c7a25cc3503070ba9f3405fa8275
ggaytan00 <coolkidgg@yahoo.com>
2019-09-20 23:39:39 -0400

    takes args instead of hardcoded

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # Amazon Price Checker
 Testing my VSC, React, Git, npm, etc. on my Windows desktop@home
+
+How to use:
+
+  node parser.js <AmazonURL> <minPrice>
+
+aka
+
+  node parser.js https://www.amazon.com/Nintendo-Switch-Bundle-Console-Joy-Super/dp/B075MV3VX2 500
--- parser.js
+++ parser.js
@@ -1,8 +1,8 @@
 const nightmare = require("nightmare")();
 
-const url =
-  "https://www.amazon.com/PetFusion-Scratcher-Cardboard-Construction-unverified/dp/B004X6UEH6?pf_rd_p=5cc0ab18-ad5f-41cb-89ad-d43149f4e286&pd_rd_wg=Aeajj&pf_rd_r=15E83G79PX7JAMTJHBGP&ref_=pd_gw_wish&pd_rd_w=8cmam&pd_rd_r=36e6f002-0ba6-4552-be73-d60c5bfcbabe";
-
+const arguments = process.argv.slice(2);
+const url = arguments[0];
+const minPrice = arguments[1];
 
 async function checkprice() {
   const priceString = await nightmare
@@ -11,7 +11,7 @@ async function checkprice() {
     .evaluate(() => document.getElementById("priceblock_ourprice").innerText)
     .end();
   const priceNumber = parseFloat(priceString.replace("$", ""));
-  if (priceNumber < 200) {
+  if (priceNumber < minPrice) {
     console.log("it is cheap!");
   } else {
     console.log("it is expensive");
